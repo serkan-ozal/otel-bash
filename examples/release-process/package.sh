85d07b84f6c179e95885977f58dd0f56b5acdb3c
Serkan ÖZAL <serkanozal86@gmail.com>
2023-07-17 09:29:27 +0300

    Add sample release process example

---
+++ examples/release-process/package.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+. ../../otel_bash.sh
+
+# or get the latest version of the "otel-bash" from remote
+# . /dev/stdin <<< "$(curl -s https://raw.githubusercontent.com/serkan-ozal/otel-bash/master/otel_bash.sh)"
+
+# or get specific version (v<version>) of the "otel-bash" from remote
+# for example, "v0.0.1" for the "0.0.1" version of the "otel-bash"
+# . /dev/stdin <<< "$(curl -s https://raw.githubusercontent.com/serkan-ozal/otel-bash/v0.0.1/otel_bash.sh)"
+
+function package_backend {
+    echo "[PACKAGE] Packaging backend app ..."
+    sleep 3
+    echo "[PACKAGE] Backend app packaging completed"
+}
+
+function package_frontend {
+    echo "[PACKAGE] Packaging frontend app ..."
+    sleep 2
+    echo "[PACKAGE] Frontend app packaging completed"
+}
+
+function package_mobile {
+    echo "[PACKAGE] Packaging mobile app ..."
+    sleep 1
+    echo "[PACKAGE] Mobile app packaging completed"
+}
+
+package_backend
+
+package_frontend
+
+package_mobile
---
+++ examples/release-process/release.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+. ../../otel_bash.sh
+
+# or get the latest version of the "otel-bash" from remote
+# . /dev/stdin <<< "$(curl -s https://raw.githubusercontent.com/serkan-ozal/otel-bash/master/otel_bash.sh)"
+
+# or get specific version (v<version>) of the "otel-bash" from remote
+# for example, "v0.0.1" for the "0.0.1" version of the "otel-bash"
+# . /dev/stdin <<< "$(curl -s https://raw.githubusercontent.com/serkan-ozal/otel-bash/v0.0.1/otel_bash.sh)"
+
+function run_build {
+    echo "[RELEASE] Building ..."
+    ./build.sh
+    echo "[RELEASE] Build completed"
+}
+
+function run_test {
+    echo "[RELEASE] Running tests ..."
+    ./test.sh
+    echo "[RELEASE] Tests completed"
+}
+
+function run_package {
+    echo "[RELEASE] Packaging ..."
+    ./package.sh
+    echo "[RELEASE] Packaging completed"
+}
+
+function run_publish {
+    echo "[RELEASE] Publishing package ..."
+    ./publish.sh
+    echo "[RELEASE] Package publish completed"
+}
+
+echo "[RELEASE] Releasing ..."
+
+run_build
+
+run_test
+
+run_package
+
+run_publish
+
+echo "[RELEASE] Release completed"
